ccdf2a651305bae3c0c27781a23e0bb52c1aa624
ekankr2 <ekankr2@naver.com>
2024-09-23 19:37:46 +0900

    사업분야까지 만듬

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,95 @@
 import Link from "next/link";
+import Image from "next/image";
+import { FaArrowCircleRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <article className="flex flex-col flex-wrap px-3.5">
+    <article className="flex flex-col flex-wrap px-3.5 ">
       <div className="w-full max-w-[384px] md:w-[712px] md:max-w-none mx-auto text-26 md:text-30">
+        <section className="pt-8 select-none text-white">
+          <div className="rounded-2xl overflow-hidden relative hover:opacity-90 main-duration-300">
+            <div className="absolute top-7 left-8 z-10  hidden text-24 md:inline-block">
+              <p>빛나는 성과와 근본적 해결을 중점으로</p>
+              <h3 className="text-36 font-bold -mt-1">
+                보다 이로운 내일을 만듭니다.
+              </h3>
+            </div>
+            <Image
+              src="/main.jpg"
+              alt="main_image"
+              className="brightness-50"
+              height={300}
+              width={712}
+            />
+            <div className="absolute text-16 font-medium bottom-6 right-6 flex justify-between flex-row items-center">
+              <p className="pr-2">회사소개 바로가기</p>
+              <span className="text-22">
+                <FaArrowCircleRight />
+              </span>
+            </div>
+          </div>
+        </section>
         <section className="pt-12">
-          <h2 className="">더 나은 세상을 만드는 혁본</h2>
-          <div className="pt-6">
-            <ul className="flex flex-wrap flex-col md:flex-row justify-between">
-              <li className="bg-gray-100 rounded-lg w-full h-[200px] md:h-[340px] md:w-[32.5%]"></li>
-              <li className="bg-gray-100 rounded-lg w-full h-[200px] mt-2 md:mt-0 md:h-[340px] md:w-[32.5%]"></li>
-              <li className="bg-gray-100 rounded-lg w-full h-[200px] md:h-[340px] mt-2 md:mt-0 md:w-[32.5%]"></li>
+          <h2 className="">혁본의 사업분야</h2>
+          <div className="pt-6 text-white">
+            <ul className="flex flex-wrap flex-col md:flex-row justify-between select-none">
+              <li className="bg-gray-100 rounded-lg text-16 overflow-hidden w-full h-[200px] md:h-[340px] relative md:w-[32.5%]">
+                <div className="absolute left-4 top-4 z-10">
+                  <p>고객 중심의 충실한 수행</p>
+                  <h3 className="text-22">분양대행</h3>
+                </div>
+                <Image
+                  className="brightness-50"
+                  src="/사업분야1.jpg"
+                  alt="분양대행이미지"
+                  width={242}
+                  height={340}
+                />
+                <div className="absolute text-14 font-medium bottom-4 right-4 flex justify-between flex-row items-center">
+                  <p className="pr-2">분양대행 보러가기</p>
+                  <span className="text-18">
+                    <FaArrowCircleRight />
+                  </span>
+                </div>
+              </li>
+              <li className="bg-gray-100 rounded-lg text-16 overflow-hidden w-full h-[200px] md:h-[340px] relative md:w-[32.5%]">
+                <div className="absolute left-4 top-4 z-10">
+                  <p>고객 중심의 충실한 수행</p>
+                  <h3 className="text-22">부동산 개발대행</h3>
+                </div>
+                <Image
+                  className="brightness-50"
+                  src="/사업분야1.jpg"
+                  alt="분양대행이미지"
+                  width={242}
+                  height={340}
+                />
+                <div className="absolute text-14 font-medium bottom-4 right-4 flex justify-between flex-row items-center">
+                  <p className="pr-2">분양대행 보러가기</p>
+                  <span className="text-18">
+                    <FaArrowCircleRight />
+                  </span>
+                </div>
+              </li>
+              <li className="bg-gray-100 rounded-lg text-16 overflow-hidden w-full h-[200px] md:h-[340px] relative md:w-[32.5%]">
+                <div className="absolute left-4 top-4 z-10">
+                  <p>고객 중심의 충실한 수행</p>
+                  <h3 className="text-22">부동산 컨설팅</h3>
+                </div>
+                <Image
+                  className="brightness-50"
+                  src="/사업분야1.jpg"
+                  alt="분양대행이미지"
+                  width={242}
+                  height={340}
+                />
+                <div className="absolute text-14 font-medium bottom-4 right-4 flex justify-between flex-row items-center">
+                  <p className="pr-2">분양대행 보러가기</p>
+                  <span className="text-18">
+                    <FaArrowCircleRight />
+                  </span>
+                </div>
+              </li>
             </ul>
           </div>
         </section>
