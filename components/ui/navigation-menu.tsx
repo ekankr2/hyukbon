ebae5a83939c106a7bb231ae482f3bb074aaa0ea
ekankr2 <ekankr2@naver.com>
2024-09-27 00:07:46 +0900

    얼라이언스

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -1,7 +1,3 @@
-// @ts-nocheck
-
-"use client";
-
 import * as React from "react";
 import * as NavigationMenuPrimitive from "@radix-ui/react-navigation-menu";
 import { cva } from "class-variance-authority";
