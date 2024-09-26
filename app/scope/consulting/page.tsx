9551d0b37fa97d55d7e0394e35937bbe58794168
ekankr2 <ekankr2@naver.com>
2024-09-26 23:28:35 +0900

    컨설팅

---
+++ app/scope/consulting/page.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Image from "next/image";
+
+function Page() {
+  return (
+    <article className="w-full px-4 md:px-0 pt-20 pb-40 max-w-[384px] md:w-[712px] lg:w-[948px] xl:w-[1316px] md:max-w-none mx-auto text-26 md:text-30">
+      <h1 className="text-28 md:text-40">CONSULTING</h1>
+      <div className="pt-4 text-16 break-normal">
+        <p>
+          고객의 이익을 보호하고
+          <br />
+          차별화된 서비스를 제공합니다
+        </p>
+      </div>
+      <div className="flex flex-row pt-20">
+        <div className="w-1/2 relative rounded-xl overflow-hidden">
+          <div className="absolute z-10 top-10 left-11 text-white text-18">
+            <h3 className="text-26">CONSULTING BUSINESS</h3>
+            <hr className="my-2" />
+            <p>
+              최고의 가치를 창출하고
+              <br />
+              고객이 원하는 해답을 찾아냅니다.
+            </p>
+          </div>
+          <Image
+            src="/consulting.jpg"
+            alt=""
+            className="w-full h-[400px] brightness-50"
+            width={5863}
+            height={3869}
+          />
+        </div>
+        <ul className="w-1/2 p-4">
+          <li className="flex flex-row items-center justify-start border-t py-4 px-8">
+            <h4 className="w-1/2">환경분석</h4>
+            <p className="text-16">
+              사업 타당성을 위한 조사
+              <br />
+              분양 전략수립 조사
+            </p>
+          </li>
+          <li className="flex flex-row items-center justify-start border-t py-4 px-8">
+            <h4 className="w-1/2">전략수립</h4>
+            <p className="text-16">
+              기업 이미지 선호 조사
+              <br />
+              건설사 및 브랜드 지표 조사
+            </p>
+          </li>
+          <li className="flex flex-row items-center justify-start border-y py-4 px-8">
+            <h4 className="w-1/2">대안제시</h4>
+            <p className="text-16">
+              상품 관련 시장 트렌드 조사
+              <br />
+              소비자 상품 선호 조사
+            </p>
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default Page;
---
+++ app/scope/research/page.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Image from "next/image";
+
+function Page() {
+  return (
+    <article className="w-full px-4 md:px-0 pt-20 pb-40 max-w-[384px] md:w-[712px] lg:w-[948px] xl:w-[1316px] md:max-w-none mx-auto text-26 md:text-30">
+      <h1 className="text-28 md:text-40">RESEARCH</h1>
+      <div className="pt-4 text-16 break-normal">
+        <p>
+          양방향 분석을 통한 객관적인 시장 판단력으로
+          <br /> 소비자에 맞춘 완벽한 전략을 개발합니다.
+        </p>
+      </div>
+      <div className="flex flex-row pt-20">
+        <div className="w-1/2 relative rounded-xl overflow-hidden">
+          <div className="absolute z-10 top-10 left-11 text-white text-18">
+            <h3 className="text-26">RESEARCH BUSINESS</h3>
+            <hr className="my-2" />
+            <p>
+              객관적인 판단을 기반으로 사업의 타당성을 검토하고
+              <br /> 혁본만의 전략 수립 및 분석을 진행합니다.
+            </p>
+          </div>
+          <Image
+            src="/research.jpg"
+            alt=""
+            className="w-full h-[500px] brightness-50"
+            width={5863}
+            height={3869}
+          />
+        </div>
+        <ul className="w-1/2 p-4">
+          <li className="flex flex-row items-center justify-start border-t py-4 px-8">
+            <h4 className="w-1/2">REAL ESTATE</h4>
+            <p className="text-16">
+              사업 타당성을 위한 조사
+              <br />
+              분양 전략수립 조사
+            </p>
+          </li>
+          <li className="flex flex-row items-center justify-start border-t py-4 px-8">
+            <h4 className="w-1/2">BRAND</h4>
+            <p className="text-16">
+              기업 이미지 선호 조사
+              <br />
+              건설사 및 브랜드 지표 조사
+            </p>
+          </li>
+          <li className="flex flex-row items-center justify-start border-y py-4 px-8">
+            <h4 className="w-1/2">TREND</h4>
+            <p className="text-16">
+              상품 관련 시장 트렌드 조사
+              <br />
+              소비자 상품 선호 조사
+            </p>
+          </li>
+        </ul>
+      </div>
+    </article>
+  );
+}
+
+export default Page;
