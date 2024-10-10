ab3f7e2900a1d24b2809c69a3ef64d93ff0c0d1b
IkHwan Im <ekankr2@naver.com>
2024-10-10 09:42:27 +0900

    문구 수정

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
               <li className="py-3 md:py-5 lg:px-5 border-b">
                 <h4 className="text-16 md:text-18">가치의 극대화</h4>
                 <p className="font-light pt-3">
-                  경험이 풍부한 임직원, 부동산 관련업계를 통한 상호 소통 중점의 유기적 업무 플로우 구축
+                  경험이 풍부한 임직원, 부동산 관련업계를 통한<br/> 상호 소통 중점의 유기적 업무 플로우 구축
                 </p>
               </li>
               <li className=" py-3 md:py-5 lg:px-5 border-b">
